ac9926d851e3212c96155d699f9e076ce6e073d1
Gwenbatta0704 <gwenaellebatta@gmail.com>
2022-10-03 15:58:04 +0200

    add order by posts

--- resources/views/partials/_order-posts.blade.php
+++ resources/views/partials/_order-posts.blade.php
@@ -5,15 +5,13 @@
         <select id="order-by"
                 name="order-by"
                 class="border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-            <option value="oldest"<?php if (isset($_GET['order-by']) && $_GET['order-by'] === 'oldest') echo ' selected' ?>>
+            <option value="oldest">
                 Oldest first
             </option>
-            <option value="newest"<?php if (!isset($_GET['order-by']) || (isset($_GET['order-by']) && $_GET['order-by'] === 'newest')) echo ' selected' ?>>
+            <option value="newest">
                 Newest first
             </option>
         </select>
-        <input type="hidden" name="action" value="index">
-        <input type="hidden" name="resource" value="">
         <button type="submit"
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md ml-1">Change
         </button>
